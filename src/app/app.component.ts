bc81e19de22865a26b6db55d0ebceaac3efc3b1b
coloz <coloz999@gmail.com>
2023-12-06 23:29:15 +0800

    更新

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { SerialService } from './serial.service';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,14 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'web-serial';
+
+  constructor(
+    private serialService: SerialService
+  ) {
+
+  }
+
+  search(){
+    this.serialService.openPort();
+  }
 }
